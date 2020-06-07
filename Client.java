99e2852700bf0d3ed93daffed66e0246c0f865cf
Meep14 <65502615+Meep14@users.noreply.github.com>
2020-06-07 17:32:34 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,66 @@
+package SERVER_SERVER_CLIENTPACKAGE;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	// big thanks to https://www.geeksforgeeks.org/socket-programming-in-java/
+	
+	private Socket socket = null;
+	private DataInputStream input = null;
+	private DataOutputStream out = null;
+	
+	
+	public Client(String address, int port) 
+	{
+		
+		try
+		{
+			socket = new Socket(address, port);
+			System.out.println("Hello world");
+			
+			input = new DataInputStream(System.in);
+			
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+	catch (IOException i) 
+		{
+		System.out.println(i);
+		}
+		
+		String line = " ";
+		
+		while (!line.equals("Over"))
+		{
+			
+			try
+			{
+				line = input.readLine(); 
+                out.writeUTF(line); 	
+		}
+			catch(IOException i )
+			{
+				System.out.println(i);
+}
+}
+		
+		
+		try
+        { 
+            input.close(); 
+            out.close(); 
+            socket.close(); 
+        } 
+		catch(IOException i)
+		{
+			System.out.println(i);
+}
+}
+	public static void main(String args[]) 
+    { 
+        Client client = new Client("127.0.0.1", 5000); 
+    } 
+}
